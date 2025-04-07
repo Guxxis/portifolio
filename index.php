8af1f850e0b1f38029d204fe401cbd39b132ec4b
Gustavo Gonçalves <gustavo.goncalves@doutoresdaweb.com.br>
2025-04-07 01:38:05 -0300

    feat: texts & style

--- index.php
+++ index.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guxxis</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <h1>Vem ai algo...</h1>
+    <main>
+        <div class="card-conteiner">
+
+        </div>
+        <div class="content-container">
+            <div class="foto-container">
+
+                <img src="image/foto-perfil.png" alt="foto de perfil" class="foto-perfil">
+            </div>
+            <div class="text-container">
+
+                <p class="text-name">Ola, eu sou o Guxxis,</p>
+        
+                <p class="text-job">Full Stack Developer</p>
+                <p class="text-job">& DevOps Enthusiast</p>
+                <p>Sou um desenvolvedor apaixonado por criar soluções completas — <strong>do front-end ao back-end</strong> — sempre com foco em qualidade de código, automações e boas práticas de deploy com <strong>DevOps</strong>.</p>
+        
+                <p>Atualmente estou aprofundando meus estudos em <strong>JavaScript</strong>, <strong>CI/CD</strong> e <strong>infraestrutura como código</strong>. Esse portfólio é onde compartilho minha trajetória, projetos e evolução contínua.</p>
+        
+                <p>Acompanhe meus projetos e trajetoria ou entre em contato</p>
+                <div class="social-midia">
+                    <a href="http://github.com/Guxxis"><i>github</i></a>
+                    <a href="https://www.linkedin.com/in/gustavogoncalves9914587/"><i>linkedin</i></a>
+                    <a href="mailto:guxxis.work@gmail.com"><i>email</i></a>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
+
 </html>
